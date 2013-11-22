857ce4c9cd85a049985fa205808f606feb765135
Climbatize <climbatize.reload@gmail.com>
2013-11-22 01:16:23 +0100

    WIP Account manager

---
+++ MyFigureCollection/src/main/java/net/myfigurecollection/authentication/Authenticator.java
@@ -0,0 +1,66 @@
+package net.myfigurecollection.authentication;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Climbatize on 22/11/13.
+ */
+public class Authenticator extends AbstractAccountAuthenticator {
+    private String TAG = "Authenticator";
+    private final Context mContext;
+
+    public Authenticator(Context context) {
+        super(context);
+
+        this.mContext = context;
+    }
+
+    @Override
+    public Bundle editProperties(AccountAuthenticatorResponse accountAuthenticatorResponse, String s) {
+        return null;
+    }
+
+    @Override
+    public Bundle addAccount(AccountAuthenticatorResponse accountAuthenticatorResponse, String s, String s2, String[] strings, Bundle bundle) throws NetworkErrorException {
+        final Intent intent = new Intent(mContext, AuthenticatorActivity.class);
+        intent.putExtra(AuthenticatorActivity.ARG_ACCOUNT_TYPE, accountType);
+        intent.putExtra(AuthenticatorActivity.ARG_AUTH_TYPE, authTokenType);
+        intent.putExtra(AuthenticatorActivity.ARG_IS_ADDING_NEW_ACCOUNT, true);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+        final Bundle opt = new Bundle();
+        opt.putParcelable(AccountManager.KEY_INTENT, intent);
+        return opt;
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, Bundle bundle) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle getAuthToken(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, String s, Bundle bundle) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public String getAuthTokenLabel(String s) {
+        return null;
+    }
+
+    @Override
+    public Bundle updateCredentials(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, String s, Bundle bundle) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle hasFeatures(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, String[] strings) throws NetworkErrorException {
+        return null;
+    }
+}
---
+++ MyFigureCollection/src/main/java/net/myfigurecollection/authentication/AuthenticatorActivity.java
@@ -0,0 +1,9 @@
+package net.myfigurecollection.authentication;
+
+import android.accounts.AccountAuthenticatorActivity;
+
+/**
+ * Created by Climbatize on 22/11/13.
+ */
+public class AuthenticatorActivity extends AccountAuthenticatorActivity {
+}
