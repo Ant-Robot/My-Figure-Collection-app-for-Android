28c01eb074719bfb69e8df161fb38e64f7c574fe
Climbatize <climbatize.reload@gmail.com>
2013-12-04 00:50:02 +0100

    started connection request

--- MyFigureCollection/src/main/java/net/myfigurecollection/api/request/ConnectionRequest.java
+++ MyFigureCollection/src/main/java/net/myfigurecollection/api/request/ConnectionRequest.java
@@ -3,23 +3,16 @@ package net.myfigurecollection.api.request;
 import android.util.Log;
 
 import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpContent;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpResponse;
-import com.google.api.client.http.MultipartContent;
 import com.google.api.client.http.UrlEncodedContent;
-import com.google.api.client.json.gson.GsonFactory;
 import com.octo.android.robospice.request.googlehttpclient.GoogleHttpClientSpiceRequest;
 
 import net.myfigurecollection.api.CollectionMode;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by Climbatize on 26/11/13.
@@ -44,20 +37,20 @@ public class ConnectionRequest extends GoogleHttpClientSpiceRequest<String> {
         GenericUrl gUrl = new GenericUrl(URL);
 
 
-        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-        nvps.add(new BasicNameValuePair("username", mUsername));
-        nvps.add(new BasicNameValuePair("password", mPassword));
-        nvps.add(new BasicNameValuePair("set_cookie", "1"));
-        nvps.add(new BasicNameValuePair("commit", "signin"));
-        nvps.add(new BasicNameValuePair("location", "http%3A%2F%2Fmyfigurecollection.net%2F"));
 
-        UrlEncodedContent content = new UrlEncodedContent();
+        Map<String, String> params = new TreeMap<String, String>();
+        params.put("username", mUsername);
+        params.put("password", mPassword);
+        params.put("set_cookie", "1");
+        params.put("commit", "signin");
+        params.put("location", "http%3A%2F%2Fmyfigurecollection.net%2F");
 
+        UrlEncodedContent content = new UrlEncodedContent(params);
 
         request = requestFactory.buildPostRequest(gUrl,content);
         HttpResponse response = request.execute();
 
 
-        return finalObject ;
+        return response.getHeaders().getCookie() ;
     }
 }
